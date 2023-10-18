5992a96a99547f6876446fba3e756e60d5efc58b
Layne <sonshi1206@gmail.com>
2023-10-18 14:03:44 +0900

    no message

--- vue-demo1/src/App.vue
+++ vue-demo1/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <!-- 头部组件 -->
+    <HmHeader></HmHeader>
+    <!-- 主体组件 -->
+    <HmMain></HmMain>
+    <!-- 底部组件 -->
+    <HmFooter></HmFooter>
+
+    <!-- 如果 HmFooter + tab 出不来 → 需要配置 vscode
+         设置中搜索 trigger on tab → 勾上
+    -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HmHeader from './components/HmHeader.vue'
+import HmMain from './components/HmMain.vue'
+import HmFooter from './components/HmFooter.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    // '组件名': 组件对象
+    HmHeader: HmHeader,
+    HmMain,
+    HmFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.App {
+  width: 600px;
+  height: 700px;
+  background-color: #87ceeb;
+  margin: 0 auto;
+  padding: 20px;
 }
 </style>
